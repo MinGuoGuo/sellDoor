693c101dae411a23f914dc9c0052dbca0f8d4865
MinGuo <923049286@qq.com>
2016-11-26 18:18:33 +0800

    路由修改

--- fe/src/entry.js
+++ fe/src/entry.js
@@ -6,8 +6,6 @@ import Index from './components/Index/Index';
 import AddUser from './components/Index/AddUser/AddUser';
 import ModifyUser from './components/Index/ModifyUser/ModifyUser';
 import Second from './components/Second/Second';
-
-
 /*
 * 以下方法是直接将组件渲染出来；下面我们看看如何用路由的形式将组件渲染出来
 * */
@@ -20,7 +18,7 @@ import Second from './components/Second/Second';
 * 将组件直接挂载在路由上；
 * */
 render((
-    <Router history={browserHistory}>
+    <Router history={hashHistory}>
         <Route path="/" component={Home}>
             <Route path="/index" component={Index} />
             <Route path="/addUser" component={AddUser} />
@@ -29,4 +27,3 @@ render((
         </Route>
     </Router>
 ), document.getElementById('root'));
-
