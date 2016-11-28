3b6a539da2d5b3ce611cfc8f9fe19f32de510bca
yuchen <18464947@qq.com>
2016-11-28 22:35:03 +0800

    list.php 添加分页功能

--- php/list.php
+++ php/list.php
@@ -2,12 +2,31 @@
 header('Access-Control-Allow-Origin: *');
 
 require_once('config.inc.php');
-// $rs = $conn->query('select * from test')->fetchAll(PDO::FETCH_ASSOC);
+
+// 页码
+// $page = $_POST['page'];
+$page = 1;
+// 每页条数
+// $pagesize = $_POST['pagesize'];
+$pagesize = 2;
+$startrow = ($page - 1)*$pagesize;
+// 查询条件
+$where = '1=1';
+
+$sqlList = 'select * from test where '.$where.' order by test_id desc limit '.$startrow.','.$pagesize;
+$sqlCount = 'select count(1) as total from test where '.$where;
+
 try {
-	$rs = $conn->query('select * from test order by test_id desc')->fetchAll(PDO::FETCH_ASSOC);
-	if($rs) {
-		echo json_encode($rs);
+	$list = $conn->query($sqlList)->fetchAll(PDO::FETCH_ASSOC);
+	$count = $conn->query($sqlCount)->fetchAll(PDO::FETCH_ASSOC);
+	$result['list']		= $list;
+	$result['count']	= $count[0]['total'];
+	$result['totalpage']= (int)ceil($pagesize/$result['count']);
+	if($result) {
+		echo json_encode($result);
+		die;
 	}
 } catch (Exception $e) {
 	echo json_encode(['msg'=>$e->getMessage(),'code'=>$e->getCode()]);
+	exit();
 }
