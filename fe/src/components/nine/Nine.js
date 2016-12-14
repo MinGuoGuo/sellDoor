c72dc82533e89d46017effc3a06089d647a13864
v_xiangmin <v_xiangmin@baidu.com>
2016-12-14 19:43:37 +0800

    ..

--- fe/src/components/nine/Nine.js
+++ fe/src/components/nine/Nine.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import $ from 'jquery';
+import 'whatwg-fetch';
 
 export default class Nine extends Component {
     constructor (props) {
@@ -8,13 +9,25 @@ export default class Nine extends Component {
     handleClick () {
         console.log(111);
         $.ajax({
-            url : '/list_student',
-            type: 'GET',
+            url : '/src/mock/list.json',
+            type: 'get',
             dataType: 'json',
             success: function (data) {
                 console.log(data);
             }
         });
+        // fetch('/src/mock/list.json', {
+        //     method: 'get',
+        //     headers: {'Content-Type': 'text/plain'},
+        //     // body: JSON.stringify({page: this.state.pageNo, pagesize: 5, name: this.state.name, age: this.state.age})
+        // }).then( (response) => {
+        //         return response.json();
+        //     }).then((result)=>  {
+        //         alert('数据加载成功');
+        //         console.log(result);
+        // }).catch((error) => {
+        //     console.log('数据加载失败');
+        // });
     }
     render () {
         return (
